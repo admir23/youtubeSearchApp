30d321b5a2e7bf0cbd2b3035f52921b8eb2387b3
admir23 <hello@admirkurtovic.com>
2018-02-20 08:06:53 +0100

    adding video select feature

--- src/index.js
+++ src/index.js
@@ -3,16 +3,23 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 const API_KEY = 'AIzaSyB7jSJZ3BdE1D71KtSd959d6m1IkUl_yuU';
 
 class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { videos:[] };
+    this.state = { 
+      videos:[],
+      selectedVideo: null
+    };
 
     YTSearch({key: API_KEY, term: 'nipsey hussle'}, (videos) => {
-      this.setState({ videos });
+      this.setState({ 
+        videos: videos,
+        selectedVideo: videos[0]
+      });
     });
   }
 
@@ -20,7 +27,10 @@ class App extends Component {
     return (
     <div>
       <SearchBar />
-      <VideoList videos={this.state.videos} />
+      <VideoDetail video={this.state.selectedVideo} />
+      <VideoList 
+        onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+        videos={this.state.videos} />
     </div>
    );
   }
